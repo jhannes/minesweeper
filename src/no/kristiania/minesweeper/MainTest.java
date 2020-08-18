89710d7b1f904eb1b84a663265954d41eb67d60b
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-08-18 15:46:03 +0200

    display mine is green, new red test display empty cell

--- src/no/kristiania/minesweeper/MainTest.java
+++ src/no/kristiania/minesweeper/MainTest.java
@@ -11,7 +11,12 @@ public class MainTest {
         assertEquals("*", displayBoard("*"));
     }
 
+    @Test
+    void itShouldDisplayEmptyCell() {
+        assertEquals("0", displayBoard("."));
+    }
+
     private String displayBoard(String s) {
-        return null;
+        return "*";
     }
 }
